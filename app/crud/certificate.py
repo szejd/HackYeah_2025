64edb935023608ff97d8f72282ee2686bfc61276
Maokx1 <4mprog@gmail.com>
2025-10-04 23:38:09 +0200

    Update issued_at field type to datetime in CertificateModel

--- app/crud/certificate.py
+++ app/crud/certificate.py
@@ -1,14 +1,14 @@
+import datetime
 from pydantic import BaseModel
-from datetime import date
 from volunteer import VolunteerModel
 from template import CertificateTemplateModel
-from datetime import date
+
 
 class CertificateModel(BaseModel):
     id: int
     volunteer_id: int
     template_id: int
     confirmed: bool
-    issued_at: date
+    issued_at: datetime.datetime
     volunteer: VolunteerModel
     template: CertificateTemplateModel
