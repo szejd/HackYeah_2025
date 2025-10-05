739633dbbbb01e72106a82deb00e5a3da2877ad8
Maokx1 <4mprog@gmail.com>
2025-10-05 05:05:22 +0200

    fix(ruff): minor import changes

--- app/db_handler/example_data.py
+++ app/db_handler/example_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 from app.crud.user import create_organisation, OrganisationCreate
 from app.crud.location import add_address, get_all_locations
+from app.db_handler.db_connection import SessionLocal
 from app.models.location import AddLocation, LocationData
 from app.schemas.db_models import User
 from app.schemas.enums import UserType, LocationType
@@ -48,8 +49,6 @@ def add_schools_as_organisations(session: Session):
         session.refresh(user_db)
         session.refresh(location)
 
-
-from app.db_handler.db_connection import SessionLocal
 
 session = SessionLocal()
 add_schools_as_organisations(session)
